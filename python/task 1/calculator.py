f21aaf402b5ab8d5e6daa52efa1590664685ad21
swordsman <yarvilyanin@noyabrsk-dobycha.gazprom.ru>
2024-06-21 14:25:22 +0500

    change calculator.py

--- python/task 1/calculator.py
+++ python/task 1/calculator.py
@@ -1,45 +1,45 @@
-""" Данная программа запрашивает у пользователя путь к файлу с арифметическими примерами, 
+""" 
+Данная программа запрашивает у пользователя путь к файлу с арифметическими примерами, 
 используя функцию eval() проводит вычисления. Результат заноситься в файл result.txt,
 ошибки в error.txt
 """
 def read_exercises_file(path_to_exercises_file):
-    """
-  Чтение файла с арифметическими примерами.
-    
-    :param path_to_exercises_file: Путь к файлу с упражнениями.
-    :return: Список строк с упражнениями.
-    """
+
+  # Чтение файла с арифметическими примерами.
+  # path_to_exercises_file: Путь к файлу с упражнениями.
+  # return: Список строк с упражнениями.
+
     with open(path_to_exercises_file, 'r') as r_file:
         exercises_list = r_file.readlines()
     return exercises_list
 
 def write_results_to_file(results_of_exercises):
-    """
-    Запись результатов упражнений в файл.
     
-    :param results_of_exercises: Словарь с результатами упражнений.
-    """
+  # Запись результатов упражнений в файл.
+    
+  # results_of_exercises: Словарь с результатами упражнений.
+    
     ns = "\n"
     with open("result.txt", "w") as w_file:
         for number, result in results_of_exercises.items():
             w_file.write(f"{number} {result} {ns}")
 
 def log_errors_to_file(exceptions):
-    """
-    Запись ошибок в файл.
     
-    :param exceptions: Словарь с ошибками.
-    """
+  # Запись ошибок в файл.
+    
+  # exceptions: Словарь с ошибками.
+    
     with open("error.txt", "w") as err_file:
         for number, error in exceptions.items():
             err_file.write(f"{number} {error} {ns}")
 
 def process_exercises(exercises_list):
-    """
-    Обработка упражнений и запись результатов.
     
-    :param exercises_list: Список строк с упражнениями.
-    """
+  # Обработка упражнений и запись результатов.
+    
+  # exercises_list: Список строк с упражнениями.
+    
     exercises_result_list = {}
     exercises_error_list = {}
     string_number = 0
